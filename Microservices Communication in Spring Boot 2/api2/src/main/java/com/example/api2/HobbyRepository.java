0c6bc595f9d91a293d581cbb8f43a55c4f795b52
thadakaajay <97282042+thadakaajay@users.noreply.github.com>
2022-02-11 22:20:28 +0530

    Add files via upload

---
+++ Microservices Communication in Spring Boot 2/api2/src/main/java/com/example/api2/HobbyRepository.java
@@ -0,0 +1,16 @@
+package com.example.api2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HobbyRepository extends JpaRepository<HobbyEntity, Integer> {
+    
+       @Query("SELECT h.name FROM HobbyEntity h WHERE h.personId=:personId")
+        public String findByPersonId(Integer personId);
+
+}
+
+
+
---
+++ Microservices Communication in Spring Boot 2/api2/src/main/java/com/example/api2/HobbyService.java
@@ -0,0 +1,21 @@
+package com.example.api2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HobbyService {
+
+    @Autowired
+    HobbyRepository hobbyRepository;
+    
+    public String findByPersonId(int personid){
+        return hobbyRepository.findByPersonId(personid);
+    }   
+    public void addHobby(HobbyEntity he){
+        hobbyRepository.save(he);
+    }
+}
+
+
+
